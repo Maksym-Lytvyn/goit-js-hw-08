0cbd13eedfa03f20eff83c8c069e85d4e2f798b0
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-03-29 17:16:09 +0300

    Друге завдання виконано)))

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
 
+console.log(iframePlayer);
+console.log(Player);
+
+iframePlayer.on('play', () => {
+    console.log('played it!');
+    
+})
+
+iframePlayer.on('pause', () => {
+    console.log('paused it!');
+})
+iframePlayer.getCurrentTime().then(function (seconds){
+    console.log(seconds);
+})
+
+
+
+window.onload = function(){
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    const parsedTime = JSON.parse(currentTime);
+    iframePlayer.setCurrentTime(parsedTime);
+}
+
+
+  iframePlayer.on('timeupdate', _throttle(function(e) {
+    console.log("time1", e);
+    localStorage.setItem('videoplayer-current-time', e.seconds)
+  }, 1000 ))  
+
+
+throt_fun();
