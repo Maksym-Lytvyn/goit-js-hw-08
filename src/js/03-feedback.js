2ef791f2bcab277a35e3c3ebe368a0629494aed8
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-03-30 17:50:54 +0300

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,11 +2,11 @@ import _throttle from 'lodash.throttle';
 
 const emailInput = document.querySelector('input[name="email"]');
 const messageInput = document.querySelector('textarea[name="message"]');
-const btn = document.querySelector('button');
+const form = document.querySelector('button');
 
 const data = {
-    email: null,
-    message: null
+    email: '',
+    message: ''
     }
 
 
@@ -19,20 +19,22 @@ messageInput.addEventListener('input', _throttle(function (event) {
     data.message = event.target.value;
     localStorage.setItem('feedback-form-state', JSON.stringify(data))
 }, 500 ))
-btn.addEventListener('click', (event) => {
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
     emailInput.value = '';
     messageInput.value = '';
     console.log(emailInput.value);
     console.log(messageInput.value);
+    localStorage.clear();
 })
 
 window.onload = function() {
     const prevResult = localStorage.getItem('feedback-form-state');
     const parsedResult = JSON.parse(prevResult);
     console.log(parsedResult);
-    if (localStorage === null) {
-        emailInput.value = null;
-        messageInput.value = null;
+    if (localStorage === '') {
+        emailInput.value = '';
+        messageInput.value = '';
     }
     else {
         emailInput.value = parsedResult.email;
